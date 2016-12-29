0f7a063d1d14553c4cc1e8091501b24e851482a2
Alexey Melezhik <melezhik@gmail.com>
2016-12-29 15:48:49 +0300

    try to active rvm int the begining

--- story.bash
+++ story.bash
@@ -1,25 +1,25 @@
 #set -x
 set -e
 
+source /usr/local/rvm/scripts/rvm 2>/dev/null || echo 'rvm not found'
+
 if rvm list|grep ruby- | grep '=*' 1>/dev/null 2>&1; then
 
   curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
-  
+
   \curl -sSL https://get.rvm.io | bash -s stable --ruby
-  
+
   source /usr/local/rvm/scripts/rvm
-  
+
   gem install bundler --no-ri --no-rdoc
 
   echo ruby version: $(ruby --version)
+
   bundler --version
   
 else
 
-  source /usr/local/rvm/scripts/rvm
-
   echo ruby version: $(ruby --version)
-
   bundler --version
 
 fi
