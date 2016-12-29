d1a2cca757d0e90eb0eb77d1f44c6b01be67ba5d
Alexey Melezhik <melezhik@gmail.com>
2016-12-29 16:23:02 +0300

    fix

--- story.bash
+++ story.bash
@@ -2,28 +2,14 @@
 set -e
 
 if /usr/local/rvm/bin/rvm list|grep ruby- | grep '=*' 1>/dev/null 2>&1; then
-
   source /usr/local/rvm/scripts/rvm
-
-  echo ruby version: $(ruby --version)
-
-  bundler --version
-
-  
 else
-
   curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
-
   \curl -sSL https://get.rvm.io | bash -s stable --ruby
-
   source /usr/local/rvm/scripts/rvm
-
   gem install bundler --no-ri --no-rdoc
-
-  echo ruby version: $(ruby --version)
-
-  bundler --version
-
-
 fi
 
+rvm --version
+ruby --version
+bundler --version
